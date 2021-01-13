934951f8bde06e405a73bb5a4bf1a55c4f72a413
Vincent-Lee <vincentflee@outlook.com>
2021-01-14 01:02:40 +1100

    save the progress for logo compo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import Tilt from 'react-tilt';
+import { FaRobot } from "react-icons/fa";
+const Logo = () => {
+  return(
+    <div className='logo'>
+      <Tilt className='Tilt'option={{max : 100}} >
+        <div className='Tilt-inner'> <a id="logo-icon" href=""> <FaRobot /> </a> </div>
+      </Tilt>
+{/* 🧠 */}
+      {/* <Tilt className='Tilt'option={{max : 25}} style={{height: 150, width:150 }}>
+        <div className='Tilt-inner'></div>
+      </Tilt> */}
+      
+    </div>
+    );
+}
+
+export default Logo;
---
+++ src/components/Nav/Navigation.js
@@ -0,0 +1,23 @@
+
+
+const Navigation = () => {
+  return(
+    <div>
+      <nav>
+        <div className='nav-L'>
+          <a href="">LOGO</a>
+        </div>
+
+        <div className='nav-R'>
+          <ul>
+            <li>
+              <a >Sign out</a>
+            </li>
+          </ul>
+        </div>
+        
+      </nav>
+    </div>)
+}
+
+export default Navigation;
